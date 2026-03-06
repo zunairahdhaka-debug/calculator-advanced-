abb05da86169211e55f355cda0402aa6f871e549
Kaisan Abudllah <zunairahdhaka@gmail.com>
2026-03-06 13:05:56 +0600

    Create style.css

---
+++ style.css
@@ -0,0 +1,81 @@
+*{
+  box-sizing:border-box;
+}
+body
+{
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  height:100vh;
+  margin:0;
+  background-color: #1e1e1e;
+  font-family:Arial,sans-serif;
+}
+.calculator {
+  background: #000;
+  border-radius: 10px;
+  box-shadow: 0 4px 20px rgba(0,0,0,0.2);
+  padding: 20px;
+  width: 320px;
+}
+
+input
+{
+  width:100%;
+  height:60px;
+  border:none;
+  border-radius:5px;
+  font-size:26px;
+  padding:10px;
+  margin-bottom:10px;
+  text-align:right;
+  background-color:#e0e0e0;
+}
+.buttons
+{
+  display:grid;
+  grid-template-columns:repeat(4,1fr);
+  gap:10px;
+  
+}
+button {
+  height: 60px;
+  font-size: 22px;
+  font-weight: bold;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  transition: background 0.3s;
+  background-color: #4CAF50;
+  color: white;
+}
+
+button:hover {
+  background-color: #45a049 !important;
+}
+
+button:active {
+  background-color: #3e8e41;
+}
+
+
+/*  Part 2 - Class 35 */
+
+button:nth-child(2), button:nth-child(3), button:nth-child(4), button:nth-child(5),button:nth-child(6), button:nth-child(7) ,button:nth-child(8) ,button:nth-child(9) ,button:nth-child(10),button:nth-child(11),button:nth-child(12),button:nth-child(13),button:nth-child(14),button:nth-child(15),button:nth-child(16),button:nth-child(18),button:nth-child(19) {
+    background-color: #333333;
+  border-radius:50%;
+}
+
+button:nth-child(1){
+    background-color: #ff0000;
+  border-radius:50%;
+}
+button:nth-child(17){
+   background-color:#0000ff;
+  border-radius:50%;
+}
+button:nth-child(20){
+  background-color:#FF5900;
+    border-radius:50%;
+ 
+}
